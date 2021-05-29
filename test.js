d1da1f0c2a5adf7b0c665497537d836b3e1a8666
Walter Ritzel <walter_ritzel@dell.com>
2021-05-28 22:10:20 -0300

    fix: limpando o package json

--- test.js
+++
@@ -1,8 +0,0 @@
-const { read, overwrite } = require('./index')
-
-var n1 = parseInt(read('Digite um numero: '))
-var n2 = parseInt(read('Digite outro numero: '))
-
-for (var i = 0; i < n1; i++) {
-  overwrite(i)
-}
